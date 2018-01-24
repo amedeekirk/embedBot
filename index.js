843d098c2801698295f0f22fc4e612459bdd8100
amedeekirk <amedeekirk@yahoo.com>
2018-01-23 23:30:30 -0800

    added non-repudiation features

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 const Discord = require('discord.js');
 const request = require('request');
 const fs = require('fs');
-//const config = require("./config.json");
+//const config = require('./config.json');
+
 
 const bot = new Discord.Client();
-
-var movieFormats = ['mov', 'mp4', 'mpeg4', 'avi', 'wmv', 'flv', '3gp', 'mpegs', 'webm' ];
 bot.login(process.env.token);
+
+const movieFormats = ['mov', 'mp4', 'mpeg4', 'avi', 'wmv', 'flv', '3gp', 'mpegs', 'webm' ];
+
 
 bot.on('message', function (message) {
 
     //Ignore other bots
-    if(message.author.bot) return;
+    if (message.author.bot) return;
 
     //Look for video attachments and download them
     var Attachment = (message.attachments).array();
-    Attachment.forEach(function(attachment) {
+    Attachment.forEach(function (attachment) {
         var filetype = attachment.filename.split('.').pop().toLowerCase();
         var filename = attachment.filename.substr(0, attachment.filename.indexOf('.'));
-        console.log(filetype);
-        if(movieFormats.includes(filetype)){
+        if (movieFormats.includes(filetype)) {
+
+            console.log('Uploading video from ' + message.author.tag + ' in ' + message.guild.name);
+
             request.get({
                 url: 'https://api.streamable.com/import?url=' + attachment.url,
                 json: true,
-                title: filename,
-                auth:{
+                data: {
+                    title: filename
+                },
+                auth: {
                     username: process.env.email,
                     password: process.env.password
                 }
             }, function (error, response, body) {
-                if(body.status === 2){
-                    message.channel.send ('https://streamable.com/' + body.shortcode);
+                if (body.status === 2) {
+                    message.channel.send('https://streamable.com/' + body.shortcode);
                 }
-                else if(body.status === 1){
+                else if (body.status === 1) {
                     console.log('video uploading, waiting...');
-                    if(getUrlRetry(body.shortcode) === 2){
-                        message.channel.send ('https://streamable.com/' + body.shortcode);
+                    if (getUrlRetry(body.shortcode) === 2) {
+                        message.channel.send('https://streamable.com/' + body.shortcode);
                     }
                 }
-                else{
-                    console.log(body);
-                    console.log('error uploading video');
+                else {
+                    //console.log(body);
+                    console.log('unusual status code found: ' + body.status);
+                    if (getUrlRetry(body.shortcode) === 2) {
+                        message.channel.send('https://streamable.com/' + body.shortcode);
+                    }
                 }
             });
         }
@@ -52,19 +61,30 @@ bot.on('message', function (message) {
             url: 'https://api.streamable.com/videos/' + shortcode,
             json: true
         }, function (error, response, body) {
-            if(body.status === 2){
+            if (body.status === 2) {
                 console.log('video done');
-                message.channel.send ('https://streamable.com/' + shortcode);
+                message.channel.send('https://streamable.com/' + shortcode);
             }
-            else if(body.status === 1){
+            else if (body.status === 1) {
                 console.log('video uploading, waiting...');
-                setTimeout(function(){getUrlRetry(shortcode);}, 1000);
+                setTimeout(function () {
+                    getUrlRetry(shortcode);
+                }, 1000);
             }
             else {
-                console.log('error uploading video');
-                console.log(body);
-                setTimeout(function(){getUrlRetry(shortcode);}, 1000);
+                console.log('unusual status code found: ' + body.status);
+                setTimeout(function () {
+                    getUrlRetry(shortcode);
+                }, 1000);
             }
         });
+    }
+
+    if(message.author.tag === 'Shuii#9701'){
+        if(message == 'list guilds'){
+            message.reply('\n' + bot.guilds.map(function (guild) {
+                return guild.name;
+            }).join('\n'));
+        }
     }
 });
